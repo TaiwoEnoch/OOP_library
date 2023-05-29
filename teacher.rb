734433bc93777c15abe51ba7afea7ffbbcdfda98
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-05-29 21:41:13 +0100

    Add teacher file as required

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(id, age, specialization, parent_permission = true, name = "Unknown")
+    super(id, age, parent_permission, name)
+    @specialization = specialization
+  end
+
+  def can_use_service
+    true
+  end
