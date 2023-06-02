22085dbe356fd7bd2b2e15081d780755ad317b61
Enoch Taiwo <111912283+TaiwoEnoch@users.noreply.github.com>
2023-06-02 13:21:58 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,34 +1,38 @@
 require_relative 'nameable'
-
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
 class Person < Nameable
-  attr_reader :id, :age
-  attr_accessor :name, :rentals
+  attr_reader :id
+  attr_accessor :name, :age, :books, :rentals
 
-  def initialize(id, age, parent_permission: true, name: 'Unknown')
-    super()
-    @id = id
+  def initialize(age, name: 'unkown', parent_permission: true)
+    super
+    @id = Random.rand(1..1001)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
   end
 
-  def correct_name
-    name
+  private
+
+  def of_age?
+    return true if @age >= 18
+
+    false
   end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
+  end
+
+  public
 
   def can_use_services?
     of_age? || @parent_permission
   end
 
-  def add_rental(rental)
-    @rentals << rental
-    rental.person = self
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
+  def correct_name
+    @name
   end
 end
