c7bbdef02e71030406f085bd41f497e6c0202e8c
Enoch Taiwo <111912283+TaiwoEnoch@users.noreply.github.com>
2023-06-02 13:19:34 +0100

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,12 +1,12 @@
 class Rental
   attr_accessor :date
-  attr_reader :person, :book
+  attr_reader :book, :person
 
-  def initialize(date, person, book)
+  def initialize(date)
     @date = date
+    @book = book
     @person = person
     person.rentals << self
-    @book = book
     book.rentals << self
   end
 end
