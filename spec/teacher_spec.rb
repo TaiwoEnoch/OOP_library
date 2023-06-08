9e20be2ac0f50ab4132acf0892748479fbcc364a
david30-maker <davidokpah396@gmail.com>
2023-06-08 12:42:15 +0100

    test for teacher.rb

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,31 +1,31 @@
-# require_relative '../person'
-# require_relative '../teacher'
+require_relative '../person'
+require_relative '../teacher'
 
-# RSpec.describe Teacher do
-#   describe '#initialize' do
-#     it 'sets the teacher attributes' do
-#       teacher = Teacher.new(name: 'Jane Smith', age: 35, specialization: 'Math')
-#       expect(teacher.name).to eq('Jane Smith')
-#       expect(teacher.age).to eq(35)
-#       expect(teacher.specialization).to eq('Math')
-#     end
-#   end
+RSpec.describe Teacher do
+  describe '#initialize' do
+    it 'sets the teacher attributes' do
+      teacher = Teacher.new(name: 'Jane Smith', age: 35, specialization: 'Math')
+      expect(teacher.name).to eq('Jane Smith')
+      expect(teacher.age).to eq(35)
+      expect(teacher.specialization).to eq('Math')
+    end
+  end
 
-#   describe '#can_use_services?' do
-#     it 'returns true' do
-#       teacher = Teacher.new(name: 'Jane Smith', age: 35, specialization: 'Math')
-#       expect(teacher.can_use_services?).to eq(true)
-#     end
-#   end
+  describe '#can_use_services?' do
+    it 'returns true' do
+      teacher = Teacher.new(name: 'Jane Smith', age: 35, specialization: 'Math')
+      expect(teacher.can_use_services?).to eq(true)
+    end
+  end
 
-#   describe '#to_h' do
-#     it 'returns the teacher details as a hash' do
-#       teacher = Teacher.new(name: 'Jane Smith', age: 35, specialization: 'Math')
-#       teacher_hash = teacher.to_h
-#       expect(teacher_hash).to be_a(Hash)
-#       expect(teacher_hash['name']).to eq('Jane Smith')
-#       expect(teacher_hash['age']).to eq(35)
-#       expect(teacher_hash['specialization']).to eq('Math')
-#     end
-#   end
-# end
+  describe '#to_h' do
+    it 'returns the teacher details as a hash' do
+      teacher = Teacher.new(name: 'Jane Smith', age: 35, specialization: 'Math')
+      teacher_hash = teacher.to_h
+      expect(teacher_hash).to be_a(Hash)
+      expect(teacher_hash['name']).to eq('Jane Smith')
+      expect(teacher_hash['age']).to eq(35)
+      expect(teacher_hash['specialization']).to eq('Math')
+    end
+  end
+end
